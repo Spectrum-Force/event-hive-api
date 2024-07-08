4dd78fdcc2d6027d58ff90ef5bb67be9fb3ddfc6
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-07-08 15:35:02 +0000

    Create the database connection

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express";
-
-
-
-
-
-
-
-
+import { dbConnection } from "./config/db.js";
 
 // create an express app
 const eventApp = express();
+
+// Create database connection
+dbConnection();
 
 //listen for an incoming request
 eventApp.listen(7070, () => {
